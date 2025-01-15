465d4524e917698ced7b644be32559b5bad34160
Woland-prj <nzimin93@gmail.com>
2025-01-15 21:22:16 +0300

    init

---
+++ cmd/sso/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/Woland-prj/microtasks_sso/internal/config"
+	"github.com/Woland-prj/microtasks_sso/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	conf := config.MustLoad()
+
+	log := mustSetupLogger(conf.Env)
+	log.Info("Starting app...")
+	log.Debug("Logger init")
+
+	// TODO: init app
+
+	// TODO: init storage
+}
+
+func mustSetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		panic("Unknown env: " + env)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
---
+++ internal/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-required:"true"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"true"`
+	GRPC        GRPCConfig    `yaml:"grpc" env-required:"true"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port" env-required:"true"`
+	Timeout time.Duration `yaml:"timeout" env-required:"true"`
+}
+
+// MustLoad trying to read config in yaml format.
+// Priority of loading: flag->env->default.
+// If not loaded panic.
+func MustLoad() *Config {
+	path := fetchConfigPath()
+
+	if path == "" {
+		panic("Config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("Config file not exist: " + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("Error while reading config: " + err.Error())
+	}
+
+	return &cfg
+}
+
+// fetchConfigPath fetching path of config and returned item.
+// Priority of loading: flag->env->default.
+// Default value is empty string.
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "Path to config file in yaml format")
+
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
